c6ef154047e79a801c9eb916a78fcfe7858935e6
gitongaisaac <gitongagisaac@gmail.com>
2023-06-14 20:08:58 +0300

    changed the module files to .js and added a package.json file to handle the module dependency

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as SymbolIdentifiers from "./modules/symbol-identifiers";
+import * as SymbolIdentifiers from "./modules/symbol-identifiers.js";
 
 /* Access the buttons on the html file */
 const button = document.querySelector(".buttons");
@@ -10,8 +10,6 @@ const operands = document.querySelectorAll(".operand");
  * =============================================================================================
  */
 /* Stores the elements pressed on the calculator interface */
-const store = () => {};
-
 const values = [];
 
 /*
@@ -145,7 +143,13 @@ const bracket_slicer = () => {
  */
 /* Does calculations on the elements in the bracket, it accepts two parameters passed from the bracket_slicer()
  * function, the new values and the outer digits of the bracket */
-const bracket_operations = (problem, outerDigits) => {};
+const bracket_operations = (problem, outerDigits) => {
+  const [indexOfStart, indexOfEnd] =
+    SymbolIdentifiers.divideSymbolIdentifier(problem);
+
+  console.log(indexOfStart);
+  console.log(indexOfEnd);
+};
 
 const operations = () => {
   console.log(values);
